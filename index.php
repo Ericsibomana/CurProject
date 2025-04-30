cab5eab6e8ce1df5c3cacfae4e754d765aa82617
Ericsibomana <ericsibomana433@gmail.com>
2025-05-01 00:30:56 +0200

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+</head>
+<body>
 <?php
 // Include data files
 require_once 'data/news.php';
@@ -5,3 +13,5 @@ require_once 'data/news.php';
 // Include base layout
 require_once 'layout.php';
 ?>
+</body>
+</html>
